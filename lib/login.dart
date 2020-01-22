f6ed4a6df330da06e28aa8603a98ce94ec1d8bf6
AlfaR0meo01 <felipero15@hotmail.com>
2020-01-21 18:52:05 -0600

    nuevos cambios

--- lib/login.dart
+++ lib/login.dart
@@ -15,12 +15,13 @@ class _Loginpage extends State<Login> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
+      
        body: Container(
          width: size.width,
          height: size.width,
          child: Stack(
            children: <Widget>[
-             Positioned(
+              Positioned(
                right: -size.width *0.22,
                top: -size.width *0.36,
                child: Circle(
@@ -31,8 +32,7 @@ class _Loginpage extends State<Login> {
                  ],
                ),
              ),
-
-             Positioned(
+              Positioned(
                left: -size.width *0.15,
                top: -size.width *0.34,
                child: Circle(
@@ -43,12 +43,86 @@ class _Loginpage extends State<Login> {
                  ],
                ),
              ),
-
-             SingleChildScrollView(
+              SingleChildScrollView(
+                child: SafeArea(
+                  child: Container(
+                  width: size.width,
+                  child: Column(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            child: Icon(
+                              Icons.account_circle,
+                              color:Colors.grey[350],
+                              size: 90,
+                            ),
+                            width: 90,
+                            height: 90,
+                            margin: EdgeInsets.only(top: size.width * .5),
+                            decoration: BoxDecoration(
+                              color:Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 25
+                                )
+                              ]
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'Bienvenidos',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300
+                            ),
+                            )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          ConstrainedBox(
+                            constraints: BoxConstraints(
+                              maxHeight: 300,
+                              maxWidth: 300,
+                            ),
+                            child: CupertinoButton(
+                              padding: EdgeInsets.symmetric(vertical: 17 ),
+                              color: Colors.pinkAccent,
+                              onPressed: (){},
+                              child: Text(
+                                'Iniciar Session',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+               ),
+             )
+           ],
+         ),
+       ),
+    );
+  }
+}
+   /*SingleChildScrollView(
                child: SafeArea(
                  child: Container(
                  width: size.width,
+                 height: size.height,
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                      Column(
                        children: <Widget>[
@@ -82,7 +156,14 @@ class _Loginpage extends State<Login> {
                           ),
                         Column(
                           children: <Widget>[
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxWidth: 350,
+                                maxHeight: 350
+                              ),
+                            ),
                             CupertinoButton(
+                              padding: EdgeInsets.symmetric(vertical: 17 ),
                               color: Colors.pink,
                               onPressed: (){},
                               child: Text('Iniciar Session'),
@@ -95,10 +176,4 @@ class _Loginpage extends State<Login> {
                   ),
                 ),
                ),
-             )
-           ],
-         ),
-       ),
-    );
-  }
-}
+             )*/
