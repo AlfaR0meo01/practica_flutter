a5bea9f38ae04c4c3cb58b680db1fd068bd20131
Vanessa Covarrubias <acovarrubias10@ucol.mx>
2020-01-30 09:49:01 -0600

    dsdasd

--- lib/vistas/main_vista.dart
+++ lib/vistas/main_vista.dart
@@ -19,43 +19,42 @@ class Vista extends StatefulWidget {
 
 class Main_visa extends State<Vista> {
   Future<List<User>> _getUser() async {
-    var data = await http.get(
-        'https://sistemas.cruzperez.com/calificaciones/flutter/get_data.php');
+    var data = await http.get('https://sistemas.cruzperez.com/calificaciones/flutter/get_data.php');
     var jsonData = json.decode(data.body);
 
     List<User> users = [];
     for (var u in jsonData) {
-      User user = User(u['id_usuario'], u['nombre'], u['apellidos'],
-          u['telefono'], u['email']);
+      User user = User(u['id_usuario'],u['nombre'], u['apellidos'], u['telefono'], u['email']);
       users.add(user);
     }
     print(users.length);
+    print('object');
     return users;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: fromHex('#2e3740'),
+      backgroundColor: fromHex('#2e3740'),
       body: Container(
         child: FutureBuilder(
           future: _getUser(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
+          builder: (BuildContext context,AsyncSnapshot snapshot) {
+            if(snapshot.data == null ){
               return Container(
                 child: Center(
                   child: Text("Cargando..."),
                 ),
               );
-            } else {
+            }else{
               return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(snapshot.data[index].nombre),
-                  );
-                },
-              );
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(snapshot.data[index].nombre),
+              ) ;
+             },
+            );
             }
           },
         ),
